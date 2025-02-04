d3d455844ef095c69803f59fdc98d92346e408c5
Neve-06 <6255234@rts-365.dk>
2025-02-04 13:08:03 +0100

    added image, base and layout files in the folder. Added variables in js, added resets in base

--- js/index.js
+++ js/index.js
@@ -2,3 +2,9 @@
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
+
+
+let wrapper = document.querySelector(".wrapper")
+let header = document.querySelector(".header")
+let footer = document.querySelector(".footer")
+
