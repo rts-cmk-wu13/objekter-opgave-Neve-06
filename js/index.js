22c352bfaf945bc99e481d48cc3a14c21f5e1773
Neve-06 <6255234@rts-365.dk>
2025-02-05 09:24:13 +0100

    added services forEach. It works! just need styling

--- js/index.js
+++ js/index.js
@@ -1,10 +1,44 @@
 // slå dig løs her... 
+let heroElm = document.querySelector(".hero")
+let heroDiv = document.createElement("div")
+let serviceElm = document.querySelector(".services")
+
+
+// section hero -------------------
+heroElm.classList.add("full-width")
+heroElm.innerHTML = `
+<img src="${hero.image}" alt="hero-image">
+`
+heroDiv.innerHTML = `
+<h2>${hero.headline}</h2>
+<p>${hero.copy}</p>
+<button><img src="${hero.icon}" alt="icon"></button>
+`
+heroElm.append(heroDiv)
+
+// section services -------------------
+
+services.forEach(service =>{
+    let serviceFigure = document.createElement("figure");
+    serviceFigure.classList.add("figure")
+    let serviceHeadline = document.createElement("h2");
+    serviceHeadline.textContent = `${service.headline}`
+    let serviceInfo = document.createElement("p")
+    serviceInfo.textContent = `${service.text}`
+    let serviceDiscover = document.createElement("p")
+    serviceDiscover.textContent = `${service.linktext}`
+    serviceFigure.append(serviceHeadline, serviceInfo, serviceDiscover)
+    serviceElm.appendChild(serviceFigure)
+})
+
+// section facilities -------------------
+
+// section sites -------------------
+
+// section advantages -------------------
+
+
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
-
-
-let wrapper = document.querySelector(".wrapper")
-let header = document.querySelector(".header")
-let footer = document.querySelector(".footer")
 
